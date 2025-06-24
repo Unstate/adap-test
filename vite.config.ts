5cb48225df7194219d869ab0b4fc343af395de7e
Fetisov <rofetisov@mail.ru>
2025-06-24 14:13:05 +0300

    chore:third test husky&lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: path.resolve(__dirname, 'index.html'),
+        }
+    }
 });
